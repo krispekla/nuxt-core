c8816479cbf1a4428b706e78d6dfc5f1877f9393
krispekla <peklarick@gmail.com>
2025-05-24 11:11:02 +0200

    feat: init setup

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/content",
+  ],
+})
