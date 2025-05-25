9cbc96e009506b1bbc34546095860ab2bf729fe2
krispekla <peklarick@gmail.com>
2025-05-25 12:45:42 +0200

    feat: format entire project

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,12 @@ import withNuxt from './.playground/.nuxt/eslint.config.mjs';
  * Extend the Nuxt config with Prettier integration
  */
 export default [
-  ...withNuxt(),
-  {
-    plugins: { prettier: require('eslint-plugin-prettier') },
-    extends: ['plugin:prettier/recommended'],
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
+	...withNuxt(),
+	{
+		plugins: { prettier: require('eslint-plugin-prettier') },
+		extends: ['plugin:prettier/recommended'],
+		rules: {
+			'prettier/prettier': 'error',
+		},
+	},
 ];
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  compatibilityDate: "2025-05-24",
+	devtools: { enabled: true },
+	compatibilityDate: '2025-05-24',
 
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
-    "@nuxt/content",
-  ],
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/image',
+		'@nuxt/scripts',
+		'@nuxt/test-utils',
+		'@nuxt/content',
+	],
 });
