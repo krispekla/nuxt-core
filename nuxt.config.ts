f499935ad9fc138a075320e9b7e325b227d934e3
krispekla <peklarick@gmail.com>
2025-05-25 21:33:17 +0200

    feat: add daisyui and color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,14 @@ export default defineNuxtConfig({
 		'@nuxt/test-utils',
 		'@nuxt/content',
 		'@nuxtjs/tailwindcss',
+		'@nuxtjs/color-mode',
 		'@pinia/nuxt',
 		'@nuxtjs/i18n',
 	],
+	colorMode: {
+		preference: 'system', // default theme
+		dataValue: 'theme', // activate data-theme in <html> tag
+		classSuffix: '',
+	},
+	tailwindcss: { exposeConfig: true },
 });
